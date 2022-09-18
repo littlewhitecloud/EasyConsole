ad0dd8b4162492ff953c4d1e304e999dccf9f0ba
Xiao_Bai_Yun <71159641+sadjok@users.noreply.github.com>
2022-09-18 17:07:28 +0800

    1.0.6 update

--- console.py
+++ console.py
@@ -1,40 +1,62 @@
-from os import getcwd, system
+# An easy console support cmd(Command).exe all function
+# Summary :
+# 	Support all cmd(Command).exe function
+#	Can add own function
 
-def cout(*text, end = ""):
-	"Console out"
+# Import
+from os import system, getcwd
+
+# Original
+def cout(*text, end = ''):
+	"Console Output"
 	for i in range(len(text)):
 		print(text[i], end = end)
-	if end != "\n":
-		print("\n")
-	
-def cin(text = ""):
+		
+	if end != '\n':
+		print('\n')
+		
+def cin(text):
+	"Console Input"
 	return input(text)
 
-__author__ = "XiaoBaiYun's Console"
-__version__ = "1.0.2"
+# Extend
+def example(*args):
+	# Do something
+	return 
 
-__about__ = """
-%s %s
-An easy program like a cmd(Command) program
-Summary:
- Support program like cmd
-	 Also can add you own rules
-""" % (__author__, __version__)
-
-_init = True # Control Console's init
+# Variable
+__version__ = "1.0.6"
+__author__ = "Xiao_Bai_Yun"
+__name__ = "Console"
+__about__ = "%s %s %s" % (__author__, __name__, __version__)
+__info__ = """
+%s
+# An easy console support cmd(Command).exe all function
+# Summary :
+# 	Support all cmd(Command).exe function
+#	Can add own function
+""" % (__about__)
+_init = True
 _exit = False
 
+# Init
 if _init:
+	system("cls")
 	system("title Console")
-	cout(__author__, __version__, end = " ")
-
+	cout(__about__)
+	
+# MainLoop
 while not _exit:
-	operator = input(getcwd() + '>')
-	if operator == "exit": # Replace the original cmd exit
+	operator = cin("PS" + ' ' +  getcwd() + '>')
+	if operator == "exit": # Replace cmd exit
 		_exit = True
-	elif operator == "about": # Add a custom "about" function
+	elif operator == "info": # Add own func
+		cout(__info__)
+	elif operator == "about": # Add own func
 		cout(__about__)
-	# Add your own rules
-	else: # Run operation in cmd
-		system(operator)
-		
+	else:
+		try:
+			system(operator)
+		except KeyboardInterrupt:
+			pass
+		"EOFError"
